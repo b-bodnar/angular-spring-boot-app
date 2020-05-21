56988f78fa63f77e99537a0410326ff09c11d0a8
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-05-21 16:14:21 +0300

    Add object Subject and subscriber for showing tasks when category is selected

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,11 +2,14 @@ import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import {Task} from "../model/Task";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
+
+  tasksSubject = new Subject<Task[]>();
 
   constructor() {
   }
@@ -15,11 +18,12 @@ export class DataHandlerService {
     return TestData.categories;
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasks() {
+    this.tasksSubject.next(TestData.tasks);
   }
 
-  getTasksByCategory(category: Category): Task[] {
-    return TestData.tasks.filter(task => task.category === category);
+  fillTasksByCategory(category: Category) {
+    const tasks = TestData.tasks.filter(task => task.category === category);
+    this.tasksSubject.next(tasks);
   }
 }
