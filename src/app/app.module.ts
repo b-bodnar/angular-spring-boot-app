0023b2ae5c3fb65d192d6e9bd30a948678d0e7b8
Bohdan Bodnar <iloveyoulviv@gmail.com>
2020-05-24 21:17:33 +0300

    Add table from mat lib with tasks. Add sorting and pagination.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CategoriesComponent} from './views/categories/categories.component';
-import {TasksComponent} from './views/tasks/tasks.component';
+import {TasksComponent} from "./views/tasks/tasks.component";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {MatTableModule} from "@angular/material/table";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -14,9 +17,13 @@ import {TasksComponent} from './views/tasks/tasks.component';
   ],
   imports: [
     BrowserModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
